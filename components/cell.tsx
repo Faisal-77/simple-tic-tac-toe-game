287df939326fe516748723b9d5c429d7ed85df3c
Faisal Alshikh <Faisalans89@gmail.com>
2023-12-27 15:05:59 +0300

    add cell components

---
+++ components/cell.tsx
@@ -0,0 +1,42 @@
+import { handleClientScriptLoad } from "next/script";
+import { Dispatch, SetStateAction } from "react";
+// const handleClick = (index) => {
+//     console.log(`Hello World ${index}`);
+
+//   }
+type cellProps = {
+  id: number;
+  go: string;
+  setGo: Dispatch<SetStateAction<string>>;
+  cells: string[];
+  setCells: Dispatch<SetStateAction<string[]>>;
+  cell: string;
+  winner: string;
+};
+const Cell = ({ go, setGo, id, cells, setCells, cell, winner}: cellProps) => {
+   
+  const handleClick = (e: any) => {
+    if (winner){
+        return
+    }
+    const notTaken = !cells[id];
+    if (notTaken) {
+      if (go === "cross") {
+        handleCellChange("cross");
+        setGo("circle");
+      } else if (go === "circle") {
+        handleCellChange("circle");
+        setGo("cross");
+      }
+    }
+  };
+  const handleCellChange = (cellToChange: string) => {
+    let copyCell = [...cells];
+    copyCell[id] = cellToChange;
+    setCells(copyCell);
+  };
+  return <div className="square" onClick={handleClick}>
+    <div className={cell}>{cell ? (cell === "cross" ? "X" : "O") : ""}</div>
+  </div>;
+};
+export default Cell;
